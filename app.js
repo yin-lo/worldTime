55195f97d764136911f98c28cfa6435013bc8690
yin-lo <yinlo@hotmail.fr>
2024-02-29 15:32:00 +0100

    ajout de dayjs et utc+timezone

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// TODO
+// module externe dayjs
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+
+// module externe express
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', { root: __dirname });
+});
+
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
